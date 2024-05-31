6d31b241247a2b04d8a2b5d5dbc2f7da9ba63b0e
Florin Romulescu <romulescu.florin.dev@gmail.com>
2024-05-31 18:30:29 +0300

    Test the socket by sending a hello message when pressing the button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect, sendMsg } from "./api"
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>React App</h1>
+        <button onClick={this.send}>Hit</button>
+      </div>
+      // <h1>App</h1>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/api/index.js
@@ -0,0 +1,23 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+let connect = () => {
+  console.log("Attempting connection...");
+  socket.onopen = () => {
+    console.log("Succesfully connected");
+  }
+
+  socket.onmessage = event => {
+    console.log("Socket closed connection: ", event);
+  }
+
+  socket.onerror = error => {
+    console.log("Socket Error: ", error);
+  }
+}
+
+let sendMsg = msg => {
+    console.log("Sending message: ", msg);
+    socket.send(msg);
+}
+
+export {connect, sendMsg};
